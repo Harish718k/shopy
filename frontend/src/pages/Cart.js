d0de27dafa834e03efa4ca75c3002bbeaa3a73ce
Harish718k <harishbca718@gmail.com>
2024-10-09 19:09:03 +0530

    revert-b56cc3c commit

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,11 +1,9 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import {Link} from 'react-router-dom'
-import {toast} from 'react-toastify'
 import './cart.css'
 
 export default function Cart({cartItems, setCartItems}){
 
-    const [complete, setComplete] = useState(false);
     function increaseQty(item){
         if(item.product.stock == item.itemQuantity){
             return;
@@ -32,19 +30,6 @@ export default function Cart({cartItems, setCartItems}){
         }
     }
 
-    function orderHandler(){
-        fetch(process.env.REACT_APP_API_URL+'/order', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(cartItems)
-        })
-        .then(()=> { 
-            setCartItems([]);
-            setComplete(true);
-            toast.success("Order Placed succesfully!")
-        })
-    }
-
     function removeItem(item){
         const updatedItem = cartItems.filter((i)=>{
             if(i.product._id !== item.product._id){
@@ -55,41 +40,41 @@ export default function Cart({cartItems, setCartItems}){
     }
 
     return cartItems.length > 0 ? <Fragment>
-        <div className="container container-fluid">
-                <h2 className="mt-5">Your Cart: <b>{cartItems.length} items</b></h2>
+        <div class="container container-fluid">
+                <h2 class="mt-5">Your Cart: <b>{cartItems.length} items</b></h2>
                 
-                <div className="row d-flex justify-content-between">
-                    <div className="col-12 col-lg-8">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-12 col-lg-8">
                         {cartItems.map((item)=>(
                             <Fragment>
                                 <hr />
-                                <div className="cart-item">
-                                    <div className="row">
-                                        <div className="col-4 col-lg-3">
+                                <div class="cart-item">
+                                    <div class="row">
+                                        <div class="col-4 col-lg-3">
                                             <img src={item.product.images[0].image} alt={item.product.name} height="90" width="115" />
                                         </div>
 
-                                        <div className="col-5 col-lg-3">
+                                        <div class="col-5 col-lg-3">
                                         <Link to={"/product/"+item.product._id}>{item.product.description}</Link>
                                         </div>
 
 
-                                        <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                                        <div class="col-4 col-lg-2 mt-4 mt-lg-0">
                                             <p id="card_item_price">${item.product.price}</p>
-                                            <p id="card_item_total_price">Total : ${(item.product.price*item.itemQuantity).toFixed(2)}</p>
+                                            <p id="card_item_total_price">Total : ${item.product.price*item.itemQuantity}</p>
                                         </div>
 
-                                        <div className="col-4 col-lg-3 mt-4 mt-lg-0">
-                                            <div className="stockCounter d-inline">
-                                                <span className="btn btn-danger minus" onClick={()=>decreaseQty(item)}>-</span>
-                                                <input type="number" className="form-control count d-inline" value={item.itemQuantity} readOnly />
+                                        <div class="col-4 col-lg-3 mt-4 mt-lg-0">
+                                            <div class="stockCounter d-inline">
+                                                <span class="btn btn-danger minus" onClick={()=>decreaseQty(item)}>-</span>
+                                                <input type="number" class="form-control count d-inline" value={item.itemQuantity} readOnly />
 
-                                                <span className="btn btn-primary plus" onClick={()=>increaseQty(item)}>+</span>
+                                                <span class="btn btn-primary plus" onClick={()=>increaseQty(item)}>+</span>
                                             </div>
                                         </div>
 
-                                        <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                                            <i id="delete_cart_item" onClick={()=>removeItem(item)} className="fa fa-trash btn btn-danger"></i>
+                                        <div class="col-4 col-lg-1 mt-4 mt-lg-0">
+                                            <i id="delete_cart_item" onClick={()=>removeItem(item)} class="fa fa-trash btn btn-danger"></i>
                                         </div>
 
                                     </div>
@@ -100,20 +85,18 @@ export default function Cart({cartItems, setCartItems}){
                         <hr />
                     </div>
 
-                    <div className="col-12 col-lg-3 my-4">
+                    <div class="col-12 col-lg-3 my-4">
                         <div id="order_summary">
                             <h4>Order Summary</h4>
                             <hr />
-                            <p>Subtotal:  <span className="order-summary-values">{cartItems.reduce((acc, item)=>(acc + item.itemQuantity),0)} (Units)</span></p>
-                            <p>Est. total: <span className="order-summary-values">${cartItems.reduce((acc, item)=>(acc + item.product.price*item.itemQuantity),0).toFixed(2)}</span></p>
+                            <p>Subtotal:  <span class="order-summary-values">{cartItems.reduce((acc, item)=>(acc + item.itemQuantity),0)} (Units)</span></p>
+                            <p>Est. total: <span class="order-summary-values">${cartItems.reduce((acc, item)=>(acc + item.product.price*item.itemQuantity),0).toFixed(2)}</span></p>
 
                             <hr />
-                            <button id="checkout_btn" className="btn btn-primary btn-block" onClick={orderHandler}>Place Order</button>
+                            <button id="checkout_btn" class="btn btn-primary btn-block">Place Order</button>
                         </div>
                     </div>
                 </div>
             </div>
-    </Fragment>:(!complete ? <Fragment><h2 className='mt-5 text-center'>Your Cart is Empty</h2></Fragment> 
-    :<Fragment><h2 className='mt-5 text-center fw-bold fs-1 text-decoration-underline'>Order Complete</h2>
-                <p className='text-center fs-2'>your order has been placed successfully</p></Fragment>)
+    </Fragment>: <h2 className='mt-5 text-center'>Your Cart is Empty</h2>
 }
